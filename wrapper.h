fe735890d9919e6bf35e14d7597c8e626e5b645e
Viresh Kumar <viresh.kumar@linaro.org>
2022-03-31 16:07:15 +0530

    initialize
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>

---
+++ wrapper.h
@@ -0,0 +1,8 @@
+#include <string.h>
+#include <xenctrl.h>
+#include <xendevicemodel.h>
+#include <xenevtchn.h>
+#include <xenforeignmemory.h>
+#include <xenstore.h>
+#include <xen/hvm/ioreq.h>
+#include <xen/io/xenbus.h>
